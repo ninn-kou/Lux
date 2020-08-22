b020fe76398eed5a479956208b3eb8aa77f37846
Hao Ren <haoren.2906@gmail.com>
2020-08-23 05:19:30 +1000

    Style updated.

--- LeetCode/344. Reverse String/344-230820.c
+++ LeetCode/344. Reverse String/344-230820.c
@@ -2,7 +2,7 @@
 // 23 August, 2020
 // 344. Reverse String
 
-void reverseString(char* s, int sSize){
+void reverseString(char* s, int sSize) {
     int head = 0;
     // The last element of string is '\0'.
     int tail = sSize - 1;
