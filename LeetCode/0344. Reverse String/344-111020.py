4ed7780743980906d0386723d31dd7846aa92a5f
Hao Ren <haoren.2906@gmail.com>
2020-10-11 17:31:00 +1100

    Structure improvements and a python solution added.

---
+++ LeetCode/0344. Reverse String/344-111020.py
@@ -0,0 +1,12 @@
+# Hao Ren
+# 11 October, 2020
+# 344. Reverse String
+# Python: One line solution. Just a joke.
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
