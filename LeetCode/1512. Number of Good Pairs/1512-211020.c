606647bcf224368e0c43e77010d435e8bce23981
Hao Ren <haoren.2906@gmail.com>
2020-10-21 19:35:57 +1100

    LeetCode No.1512 added.

---
+++ LeetCode/1512. Number of Good Pairs/1512-211020.c
@@ -0,0 +1,16 @@
+// Hao Ren
+// 21 October, 2020
+// 1512. Number of Good Pairs
+
+int numIdenticalPairs(int* nums, int numsSize){
+    int good_pairs = 0;
+    for (int i = 0; i < numsSize; i++) {
+        // Start from the next element.
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] == nums[j]) {
+                good_pairs += 1;
+            }
+        }
+    }
+    return good_pairs;
+}
