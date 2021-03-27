13110fc8b99420de4a7ebeb11becbc922363c597
Hao Ren <haoren.2906@gmail.com>
2021-03-28 05:44:07 +1100

    Update: LeetCode No.709

--- ALGO_LeetCode/0709. To Lower Case/709-260820.c
+++ ALGO_LeetCode/0709. To Lower Case/709-260820.c
@@ -2,6 +2,8 @@
 // 26 August, 2020
 // 709. To Lower Case
 
+#define TOLOWER 'a' - 'A'
+
 // Eg: In ASCII table, char 'a' = 97, and char 'A' = 65.
 // To converse upper case to lower case, we need to add 32, which is the
 // difference between 'a' and 'A'.
@@ -10,7 +12,7 @@ char *toLowerCase(char *str) {
     int n = 0;
     while (str[n] != '\0') {
         if (str[n] >= 'A' && str[n] <= 'Z') {
-            str[n] += ('a' - 'A');
+            str[n] += TOLOWER;
         }
         n++;
     }
