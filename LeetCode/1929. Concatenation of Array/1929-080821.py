b742aa1a94608357d93a1b694427d18377f8ed45
Hao Ren <haoren.2906@gmail.com>
2021-08-08 09:33:35 +1000

    LeetCode No.1929 added.

---
+++ LeetCode/1929. Concatenation of Array/1929-080821.py
@@ -0,0 +1,13 @@
+"""
+Hao Ren
+08 August, 2021
+1929. Concatenation of Array
+"""
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums + nums
