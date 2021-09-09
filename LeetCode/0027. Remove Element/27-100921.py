3f8f981ee9735f984c4ec7e3825f3d16de0c7198
Hao Ren <haoren.2906@gmail.com>
2021-09-10 07:30:32 +1000

    LeetCode No.27 added.

---
+++ LeetCode/0027. Remove Element/27-100921.py
@@ -0,0 +1,25 @@
+class Solution_One(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                del nums[i]
+                i -= 1
+            i += 1
+        return len(nums)
+
+class Solution_Two(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        while val in nums:
+            nums.remove(val)
+        return len(nums)
