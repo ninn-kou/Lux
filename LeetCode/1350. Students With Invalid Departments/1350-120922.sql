a699ce75efd6ba7b97b1209f5a38223fa7897e32
Hao Ren <haoren.2906@gmail.com>
2022-09-12 14:22:38 +1000

    LeetCode No.1350 added.

---
+++ LeetCode/1350. Students With Invalid Departments/1350-120922.sql
@@ -0,0 +1,16 @@
+/**
+ * Hao Ren
+ * 17 April, 2022
+ *1350. Students With Invalid Departments
+ */
+
+SELECT
+    id, name
+FROM
+    Students
+WHERE
+    department_id NOT IN (
+        SELECT
+            id
+        FROM
+            Departments)
